015abc5024231150da2699b888167960aa34044a
cristian <cristian.wilson3@gmail.com>
2022-01-22 02:53:27 -0300

    codigo calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+const numbers = document.getElementsByClassName(`js-number`);
+const btnOne = document.getElementById(`btn`);
+
+// const mostrarNumero = () => {
+//   console.log(Number(numbers.));
+// };
+
+// numbers.forEach((element) => {
+//   addEventListener(`click`, () => {
+//     console.log(element.innerHTML);
+//   });
+// });
+
+console.log(numbers);
